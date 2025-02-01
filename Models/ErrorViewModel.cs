b5d87fab586e8b7897a97b1bda46538275bbdfd3
Suman Kanuganti <kanug-su@de001.ittgr.net>
2025-02-01 12:24:48 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureDeploymentWithGitActions.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
